26bde75d89103b0f81788ac9cdf6ed99e4628054
Dean Hardy <deanhardy@gmail.com>
2023-01-18 10:39:41 -0500

    updated panel figure of maps

--- data-retrieval.R
+++ data-retrieval.R
@@ -63,10 +63,16 @@ bg2 <- bg %>%
 
 st <- get_acs(geography = 'county',
               state = c('MS'),
-              variables = "B03002_001E",
+              variables = VAR,
               year = 2020,
               geometry = TRUE,
-              output = 'wide')
+              output = 'wide') %>%
+  mutate(mnhhinc = agghhinc/hu,
+         propPOC = 1 - (white/total),
+         medhhinc = medhhinc/1000,
+         sqkm_cnty = as.numeric(st_area(geometry)) / 1e6) %>%
+  mutate(popd = total/sqkm_cnty,
+         total = total/1000)
 
 
 ##import area of interest data
@@ -80,7 +86,7 @@ AOI <- st_read(file.path(datadir, '/MAP_generalized_regions/MAP_generalized_regi
 tmap_mode(mode = c('plot'))
 
 map <- tm_shape(st) + 
-  tm_polygons(col = 'white') + 
+  tm_polygons() + 
   tm_shape(AOI) +
   tm_borders(col = 'red', lwd = 2) + 
   tm_compass(position = c(0.11,0.12),
@@ -91,10 +97,84 @@ map <- tm_shape(st) +
   tm_layout(inner.margins = c(0.1,0.05,0.05,0.05))
 map
 
-jpeg(paste0(datadir, "/yazoo-map.jpeg"), 
-     units = 'in', 
-     res = 300, 
-     height = 3, 
-     width = 2)
-map
+# jpeg(paste0(datadir, "/yazoo-map.jpeg"), 
+#      units = 'in', 
+#      res = 300, 
+#      height = 3, 
+#      width = 2)
+# map
+# dev.off()
+
+## population map
+pop <- tm_shape(st) + 
+  tm_polygons(col = 'popd', 
+              palette = 'Greys',
+              title = 'Population Density (per KM^2)',
+              # style = 'cont',
+              legend.is.portrait = FALSE) + 
+  tm_shape(AOI) +
+  tm_borders(col = 'red', lwd = 2) + 
+  # tm_compass(position = c(0.11,0.12),
+  #            size = 1.1) +
+  # tm_scale_bar(position = c('left', 'bottom'),
+  #              breaks = c(0,100),
+  #              text.size = 0.5) +
+  tm_layout(inner.margins = c(0.05,0.05,0.05,0.05),
+            # legend.position = c('left', 'BOTTOM'),
+            legend.outside.position = "bottom",
+            legend.outside.size = 0.35,
+            legend.outside = TRUE
+  )
+pop
+
+## proportion POC map
+race <- tm_shape(st) + 
+  tm_polygons(col = 'propPOC', palette = '-viridis',
+              title = 'Proportion People of Color',
+              # style = 'cont',
+              legend.is.portrait = FALSE) + 
+  tm_shape(AOI) +
+  tm_borders(col = 'red', lwd = 2) + 
+  # tm_compass(size = 1.1,
+  #            position = c('right', 'bottom')) +
+  # tm_scale_bar(position = 'left',
+  #              breaks = c(0,50,100),
+  #              text.size = 0.5) + 
+  tm_layout(inner.margins = c(0.1,0.05,0.05,0.05),
+            # legend.position = c('left', 'BOTTOM'),
+              legend.outside.position = "bottom",
+              legend.outside.size = 0.35,
+              legend.outside = TRUE
+            )
+race
+
+## median hh income map
+inc <- tm_shape(st) + 
+  tm_polygons(col = 'medhhinc', palette = 'YlOrBr', 
+              title = 'Median Income (x $1000)',
+              # style = 'cont',
+              legend.is.portrait = FALSE) + 
+  tm_shape(AOI) +
+  tm_borders(col = 'red', lwd = 2) + 
+  tm_compass(position = c('left', 'bottom'),
+             size = 1.1) +
+  tm_scale_bar(position = c('left', 'bottom'),
+               breaks = c(0,100),
+               text.size = 0.5) +
+  tm_layout(inner.margins = c(0.05,0.05,0.05,0.05),
+            # legend.position = c('left', 'BOTTOM'),
+            legend.outside.position = "bottom",
+            legend.outside.size = 0.35,
+            legend.outside = TRUE
+  )
+inc
+
+## create panel figure
+jpeg(paste0(datadir, "/yazoo-panel.jpeg"),
+     units = 'in',
+     res = 300,
+     height = 3.5,
+     width = 6.5)
+tmap_arrange(pop, race, inc, ncol = 3, nrow = 1)
 dev.off()
+
